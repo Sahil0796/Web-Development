a160d8eef409dd8c8b60fb17d68c2c2ce2651635
Sahil0796 <closedsafe@gmail.com>
2023-11-20 13:00:15 +0530

    Updated

---
+++ VIDEO 54 - JavaScript & Installing Node.js/new.js
@@ -0,0 +1,4 @@
+console.log("Hello World !");
+console.log("the code is running....... !");
+console.log("Wow the code is running like wow ....... !");
+console.log("Wow the code is running awesomely ....... !");
---
+++ VIDEO 54 - JavaScript & Installing Node.js/script.js
@@ -0,0 +1,32 @@
+
+alert("Hello JavaScript !");
+
+console.log("Hello World !");
+console.log("the code is running....... !");
+console.log("Wow the code is running like wow ....... !");
+console.log("Wow the code is running awesomely ....... !");
+
+document.write("Wow the code is running....... !");
+
+var a = prompt("Enter your number here !");
+
+console.log("Your number is " + a );
+
+var isTrue = confirm("Are you sure you want to leave !");
+
+if(isTrue){
+    console.log("You are leaving.....");
+}
+else{
+    console.log("You are not leaving");
+}
+
+ document.title = " This is Node.js ! "
+
+ document.body.style.backgroundColor = "black";
+ document.body.style.color = "white";
+ 
+
+
+
+
