5ba282ba4a53442b658f439f0c40eb4740ef9aa9
Sahil0796 <closedsafe@gmail.com>
2023-11-29 15:29:30 +0530

    updated

--- VIDEO 59 - Faulty Calculator/index.js
+++ VIDEO 59 - Faulty Calculator/index.js
@@ -3,3 +3,5 @@ console.log("Faulty Calculator !");
 console.log("Hello Faulty Calculator !");
 
 console.log("Hello Again Faulty Calculator !");
+
+console.log("Hello for the third time Faulty Calculator !");
