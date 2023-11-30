c6f548aaeb08b7c83d4362589255c0e33905ceb2
Sahil0796 <closedsafe@gmail.com>
2023-11-30 13:42:27 +0530

    Updated

--- VIDEO 60 - JavaScript Strings/index.js
+++ VIDEO 60 - JavaScript Strings/index.js
@@ -1,3 +1,51 @@
+// JAVASCRIPT IS ALSO KNOWN AS FORGIVING LANGUAGE AS IT THROWS LESS ERRORS !
+
 console.log("JavaScript Strings !");
 
-console.log("Hello Again JavaScript Strings !");
+let Name = "Sahil Bhandari";
+
+// console.log(Name[0]);
+// console.log(Name[1]);
+// console.log(Name[2]);
+// console.log(Name[3]);
+// console.log(Name[4]);
+// console.log(Name[5]);
+// console.log(Name[6]);
+// console.log(Name[7]);
+// console.log(Name[8]);
+// console.log(Name[9]);
+// console.log(Name[10]);
+// console.log(Name[11]);
+// console.log(Name[12]);
+// console.log(Name[13]);
+
+console.log(Name.length);
+
+let my_name = "Sahil Bhandari";
+let my_friend = "Tony";
+
+console.log(`My name is ${my_name} and my friend is ${my_friend}`);    // template literals
+
+console.log(my_name.toLowerCase());
+console.log(my_name.toUpperCase());
+console.log(my_name.length);
+console.log(my_name.slice(2, 8));    // 2 included and 8 not included.
+console.log(my_name.slice(2));       // 2 is index, starting from 2 till end will be included.
+console.log(my_friend.replace("To", "Mo"));    // Only first occurrence will get replace. 
+console.log(my_name.concat(my_friend, "Smith ", "Gilly")); 
+console.log(my_name.trim());  
+
+// STRINGS ARE IMMUTABLE. IN ORDER TO ACCESS THE CHARACTER AT AN INDEX WE USE THE FOLLOWING SYNTAX : -
+
+let f_name = "Sahil";
+
+console.log(f_name[0]);
+console.log(f_name[1]);
+
+
+
+
+
+
+
+
