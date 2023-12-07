7779ebb6dc4f5eb8f2320b08c3f67e1ce96d47fa
Sahil0796 <closedsafe@gmail.com>
2023-12-07 12:50:07 +0530

    Updated

---
+++ VIDEO 68 - JavaScript - Selecting by Ids, Classes, and More/script.js
@@ -0,0 +1,11 @@
+
+let boxes = document.getElementsByClassName("box");    // Use of getElementByClassName to target the elements of that class. Elements for class.
+console.log(boxes);
+
+boxes[3].style.backgroundColor = "brown";
+boxes[3].style.color = "white";
+
+document.getElementById("Newbox").style.backgroundColor = "green"; // Use of getElementById to target the elements of that ID. Element for ID.
+document.getElementById("Newbox").style.color = "white";
+
+
