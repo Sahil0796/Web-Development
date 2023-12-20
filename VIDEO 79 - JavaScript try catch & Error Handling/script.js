33f26adc1ee4a1076a52718c559cc25c9c71c086
Sahil0796 <closedsafe@gmail.com>
2023-12-20 14:20:53 +0530

    Updated

---
+++ VIDEO 79 - JavaScript try catch & Error Handling/script.js
@@ -0,0 +1,6 @@
+let a = prompt("Enter the first number");      // Prompt return us the String.
+let b = prompt("Enter the second number");
+
+let sum = parseInt(a) + parseInt(b);           // To get the result in an integer value, use parseint().
+
+console.log("This is the total of your numbers", sum );
