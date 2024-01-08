3a0b3e3f3795844bb8be0da3dbaa7da778d1b678
Sahil0796 <closedsafe@gmail.com>
2024-01-08 13:12:38 +0530

    Updated

--- VIDEO 85 - Backend, Node.js & npm/server.js
+++ VIDEO 85 - Backend, Node.js & npm/server.js
@@ -1,1 +1,13 @@
-console.log("Hello Nodejs !");
+
+// Nodejs runs JavaScript on the backend side.
+
+var slugify = require('slugify')
+
+let a = slugify('some string') // some-string
+console.log(a);
+
+
+// if you prefer something other than '-' as separator
+const b = slugify('some string', '_')  // some_string
+console.log(b);
+
