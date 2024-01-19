930a9a62955ef1673740cdbf81541e9321ce60e1
Sahil0796 <closedsafe@gmail.com>
2024-01-19 13:47:46 +0530

    Updated

--- VIDEO 95 - MongoDB - CRUD Operations/crud.mongodb.js
+++ VIDEO 95 - MongoDB - CRUD Operations/crud.mongodb.js
@@ -2,80 +2,88 @@ use("SB")
 
 // console.log("SB");
 
-db.createCollection("Chapters");
+// db.createCollection("Chapters");
 
-db.Chapters.insertOne({
-    Name: "SAHIL",
-    Age: 27,
-    Role: "Developer",
-    Shift: "Morning"
-})
+// INSERTING DATA !
 
 
+// db.Chapters.insertOne({
+//     Name: "SAHIL",
+//     Age: 27,
+//     Role: "Developer",
+//     Shift: "Morning"
+// })
 
-db.Chapters.insertMany([
-    {
-        "Name": "SAHIL",
-        "Age": 27,
-        "Role": "Developer",
-        "Shift": "Morning"
-    },
-    {
-        "Name": "JESSICA",
-        "Age": 30,
-        "Role": "Designer",
-        "Shift": "Afternoon"
-    },
-    {
-        "Name": "MIKE",
-        "Age": 25,
-        "Role": "Tester",
-        "Shift": "Evening"
-    },
-    {
-        "Name": "EMMA",
-        "Age": 28,
-        "Role": "Analyst",
-        "Shift": "Morning"
-    },
-    {
-        "Name": "ALEX",
-        "Age": 32,
-        "Role": "Manager",
-        "Shift": "Day"
-    },
-    {
-        "Name": "LISA",
-        "Age": 29,
-        "Role": "Developer",
-        "Shift": "Morning"
-    },
-    {
-        "Name": "RYAN",
-        "Age": 26,
-        "Role": "Tester",
-        "Shift": "Night"
-    },
-    {
-        "Name": "SOFIA",
-        "Age": 31,
-        "Role": "Designer",
-        "Shift": "Afternoon"
-    },
-    {
-        "Name": "DAVID",
-        "Age": 33,
-        "Role": "Analyst",
-        "Shift": "Evening"
-    },
-    {
-        "Name": "OLIVIA",
-        "Age": 28,
-        "Role": "Manager",
-        "Shift": "Day"
-    }
-]
-)
+
+
+// db.Chapters.insertMany([
+//     {
+//         "Name": "SAHIL",
+//         "Age": 27,
+//         "Role": "Developer",
+//         "Shift": "Morning"
+//     },
+//     {
+//         "Name": "JESSICA",
+//         "Age": 30,
+//         "Role": "Designer",
+//         "Shift": "Afternoon"
+//     },
+//     {
+//         "Name": "MIKE",
+//         "Age": 25,
+//         "Role": "Tester",
+//         "Shift": "Evening"
+//     },
+//     {
+//         "Name": "EMMA",
+//         "Age": 28,
+//         "Role": "Analyst",
+//         "Shift": "Morning"
+//     },
+//     {
+//         "Name": "ALEX",
+//         "Age": 32,
+//         "Role": "Manager",
+//         "Shift": "Day"
+//     },
+//     {
+//         "Name": "LISA",
+//         "Age": 29,
+//         "Role": "Developer",
+//         "Shift": "Morning"
+//     },
+//     {
+//         "Name": "RYAN",
+//         "Age": 26,
+//         "Role": "Tester",
+//         "Shift": "Night"
+//     },
+//     {
+//         "Name": "SOFIA",
+//         "Age": 31,
+//         "Role": "Designer",
+//         "Shift": "Afternoon"
+//     },
+//     {
+//         "Name": "DAVID",
+//         "Age": 33,
+//         "Role": "Analyst",
+//         "Shift": "Evening"
+//     },
+//     {
+//         "Name": "OLIVIA",
+//         "Age": 28,
+//         "Role": "Manager",
+//         "Shift": "Day"
+//     }
+// ]
+// )
+
+
+
+// READING DATA !
+
 
 // let a = db.Chapters.find({Age: 27});
 // console.log(a);
@@ -86,6 +94,40 @@ db.Chapters.insertMany([
 
 // let b = db.Chapters.findOne({Age: 27});
 // console.log(b);
+
+
+
+// UPDATING DATA !
+
+
+// db.Chapters.updateOne({Shift: "Morning"}, {$set: {Shift: "Evening"}});
+
+// db.Chapters.updateMany({Shift: "Morning"}, {$set: {Shift: "Evening"}});
+
+
+
+// DELETING THE DATA !
+
+
+// db.Chapters.deleteOne({Age: 33});
+
+
+db.Chapters.deleteMany({Shift: "Evening"});
+
+
+// MONGODB OPERATORS : https://www.mongodb.com/docs/manual/reference/operator/query/  
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
