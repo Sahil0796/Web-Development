bcc6ac992ed81d281d8385d10ef7945f75833955
Sahil0796 <closedsafe@gmail.com>
2024-05-11 20:49:00 +0530

    Done

---
+++ VIDEO 110 - Conditional Rendering  & Rendering Lists in React/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  const [count, setCount] = useState(0)
+  const [showbtn, setshowbtn] = useState(true)
+
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      
+      {showbtn?<button>showbtn is true !</button>:
+      <button>showbtn is false !</button>}
+      
+      {/* {showbtn && <button>showbtn is true !</button>} */}
+      <div className="card">
+        <button onClick={() => setshowbtn(!showbtn)}>
+          Toggle showbtn
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
