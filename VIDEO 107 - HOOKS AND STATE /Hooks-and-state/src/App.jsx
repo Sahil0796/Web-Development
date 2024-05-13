ddbbcc72549e92d3eb6617ac5c512af954ba4971
Sahil0796 <closedsafe@gmail.com>
2024-05-13 14:46:25 +0530

    Done

--- VIDEO 107 - HOOKS AND STATE /Hooks-and-state/src/App.jsx
+++ VIDEO 107 - HOOKS AND STATE /Hooks-and-state/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,9 +6,13 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  useEffect(() => {
+    alert("The count has changed !");
+  }, [count])
+  
+
   return (
     <>
-     
       <div>This is count {count}</div>
       <button onClick={()=>{setCount(count + 1)}} >Upadte Count</button>
 
