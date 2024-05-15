18796e5b798bb94e412aea8818d3f1b3c281ce04
Sahil0796 <closedsafe@gmail.com>
2024-05-15 12:17:48 +0530

    Done

---
+++ VIDEO 113 - Display the cards/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  const [cards, setCards] = useState([])
+
+  const fetchData = async () => {
+    let a = await fetch("https://jsonplaceholder.typicode.com/posts")
+    let data = await a.json()
+    setCards(data)
+    console.log(data)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+
+  return (
+    <>
+      <Navbar/>
+
+      <div className="container">
+
+        {cards.map((card) => {
+
+          return <div key={card.id} className="card">
+            <h1>{card.title}</h1>
+            <p>{card.body}</p>
+            <span>By: UserId: {card.Id}</span>
+
+          </div>
+
+        })}
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
