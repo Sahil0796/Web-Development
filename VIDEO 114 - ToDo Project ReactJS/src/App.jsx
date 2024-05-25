a433ce4478c306685bb45b1d7a3cdcf110c9c863
Sahil0796 <closedsafe@gmail.com>
2024-05-25 14:44:59 +0530

    Done

--- VIDEO 114 - ToDo Project ReactJS/src/App.jsx
+++ VIDEO 114 - ToDo Project ReactJS/src/App.jsx
@@ -23,14 +23,19 @@ function App() {
   }
 
 
-  const handleEdit = () => {
-    
+  const handleEdit = (e, id) => {
+    let t = todos.filter(i => i.id === id)
+    setTodo(t[0].todo)
+    let newTodos = todos.filter(item => {
+      return item.id !== id
+    });
+    setTodos(newTodos)
   }
 
 
   const handleDelete = (e, id) => {
-    let newTodos = todos.filter(item=>{
-      return item.id!==id
+    let newTodos = todos.filter(item => {
+      return item.id !== id
     });
     setTodos(newTodos)
 
@@ -39,7 +44,7 @@ function App() {
 
   const handleCheckbox = (e) => {
     let id = e.target.name;
-    let index = todos.findIndex(item=>{
+    let index = todos.findIndex(item => {
       return item.id === id;
     })
     let newTodos = [...todos];
@@ -54,34 +59,39 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className="container mx-auto my-5 rounded-xl p-5 bg-orange-400 min-h-[80vh] min-w-[]">
+      <div className="container mx-auto my-5 rounded-xl p-5 bg-orange-400 min-h-[80vh]">
 
         <div className="addTodo my-5">
 
           <h2 className='text-lg font-bold'>Add a ToDo</h2>
 
-          <input onChange={handleChange} value={todo} type="text" className='w-80' />
+          <input onChange={handleChange} value={todo} type="text" className='w-1/3' />
 
-          <button onClick={handleAdd} className='bg-black p-3 py-1 text-white rounded-md mx-5 font-bold'>Add</button>
+          <button onClick={handleAdd} className='bg-black p-3 py-1 text-white rounded-md mx-5 font-bold'>Save</button>
 
         </div>
 
         <h2 className='text-xl font-bold'>Your Todos</h2>
 
         <div className='todos'>
+          {todos.length === 0 && <div className='m-5'>No ToDos to Display !</div>}
 
           {todos.map(item => {
 
-            return <div key={item.id} className="todo flex my-5 justify-between w-1/4">
+            return <div key={item.id} className="todo flex my-4 justify-between items-center w-1/2">
 
-              <input name={item.id} onChange={handleCheckbox} type="checkbox" value={item.isCompleted} id="" />
+              <div className='flex gap-5'>
 
-              <div className={item.isCompleted?"line-through":""}>{item.todo}</div>
+                <input name={item.id} onChange={handleCheckbox} type="checkbox" value={item.isCompleted} id="" />
+
+                <div className={item.isCompleted ? "line-through" : ""}>{item.todo}</div>
+
+              </div>
 
               <div className="buttons">
 
-                <button onClick={handleEdit} className='bg-black p-3 py-1 text-white rounded-md mx-2 font-bold'>Edit</button>
-                <button onClick={(e)=>{handleDelete(e, item.id)}} className='bg-black p-3 py-1 text-white rounded-md mx-2 font-bold'>Delete</button>
+                <button onClick={(e) => handleEdit(e, item.id)} className='bg-black p-3 py-1 text-white rounded-md mx-1 font-bold'>Edit</button>
+                <button onClick={(e) => { handleDelete(e, item.id) }} className='bg-black p-3 py-1  text-white rounded-md mx-1 font-bold'>Delete</button>
 
               </div>
             </div>
