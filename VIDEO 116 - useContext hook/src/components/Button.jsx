7c0005efc8b3805b38f4ea33d516babbe6b826c6
Sahil0796 <closedsafe@gmail.com>
2024-05-28 12:06:50 +0530

    Done

---
+++ VIDEO 116 - useContext hook/src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Component1 from './Component1'
+
+
+const Button = () => {
+    return (
+        <div>
+            <button>
+                <span><Component1/></span>Enter
+            </button>
+        </div>
+    )
+}
+
+export default Button
---
+++ VIDEO 116 - useContext hook/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from './Button'
+
+const Navbar = () => {
+  return (
+    <>
+    <div>
+        Navbar
+    </div>
+    <button/>
+    </>
+  )
+}
+
+export default Navbar
