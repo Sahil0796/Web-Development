1ab40b5c26d9b31646c8b677ac699410983fe5c3
Sahil0796 <closedsafe@gmail.com>
2024-05-30 14:46:21 +0530

    Done

---
+++ VIDEO 63.1 - map() in JavaScript/index.js
@@ -0,0 +1,14 @@
+
+
+Numbers = [1,2,3,4,5,6,7,8,9,10];
+
+// console.log(Numbers);
+
+ double = (value, index, arr) =>{
+    return value * 2;
+}
+
+const numberDoubles = Numbers.map(double);
+
+console.log(numberDoubles);
+
